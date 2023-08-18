c33259559497a15242f0b0323bc5b992216d5895
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-08-19 04:38:10 +0530

    Change browser router to hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,7 +9,7 @@ import '@fontsource/roboto/700.css';
 import Home from './Home.tsx';
 import Second from './Second.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Home />,
