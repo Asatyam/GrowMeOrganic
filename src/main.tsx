6bb4dfe09948b53a26071373511a5a78aab67c3f
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-08-19 04:50:05 +0530

    Change base href

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter,createBrowserRouter, RouterProvider } from 'react-router-dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,7 +9,7 @@ import '@fontsource/roboto/700.css';
 import Home from './Home.tsx';
 import Second from './Second.tsx';
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
